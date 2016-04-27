d806f0d434eeb8566ccc8d7b6359b2c31521826e
JohanSvensson <js@resolutiongames.com>
2016-04-27 15:01:24 +0200

    Add basic signal implementation.

---
+++ Assets/Script/Example/SignalListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SignalListener : MonoBehaviour {
+
+	void Start () {
+        SignalSender.TestSignal.AddListener(OnSignal);
+        SignalSender.TestSignal.AddListener(OnSignal);
+    }
+    
+    private void OnSignal(string text, int number) {
+        Debug.Log(ToString() + " GOT NOTIFICATION -> text: " + text + "   number: " + number);
+    }
+
+    void cannor() { }
+
+}
---
+++ Assets/Script/Signals/Signal.cs
@@ -0,0 +1,132 @@
+﻿using System.Linq;
+
+namespace CakewalkIoC.Signal {
+
+    /// <summary>
+    /// Basic signal with one parameter.
+    /// </summary>
+    public class Signal<T> {
+
+        public delegate void SignalDelegate(T item);
+        private event SignalDelegate SendSignal;
+
+        /// <summary>
+        /// Add a unique listener to the signal.
+        /// </summary>
+        public void AddListener(SignalDelegate listener) {
+            if(SendSignal == null) {
+                SendSignal += listener;
+            }
+            else if(!SendSignal.GetInvocationList().Contains(listener)) {
+                SendSignal += listener;
+            }
+        }
+
+        public void RemoveListener(SignalDelegate listener) {
+            SendSignal -= listener;
+        }
+
+        public void Dispatch(T item) {
+            if(SendSignal != null) {
+                SendSignal(item);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Signal with 2 parameters.
+    /// </summary>
+    public class Signal<T, U> {
+
+        public delegate void SignalDelegate(T item1, U item2);
+        private event SignalDelegate SendSignal;
+
+        /// <summary>
+        /// Add a unique listener to the signal.
+        /// </summary>
+        public void AddListener(SignalDelegate listener) {
+            if(SendSignal == null) {
+                SendSignal += listener;
+            }
+            else if(!SendSignal.GetInvocationList().Contains(listener)) {
+                SendSignal += listener;
+            }
+        }
+
+        public void RemoveListener(SignalDelegate listener) {
+            SendSignal -= listener;
+        }
+
+        public void Dispatch(T item1, U item2) {
+            if(SendSignal != null) {
+                SendSignal(item1, item2);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Signal with 3 parameters.
+    /// </summary>
+    public class Signal<T, U, V> {
+
+        public delegate void SignalDelegate(T item1, U item2, V item3);
+        private event SignalDelegate SendSignal;
+
+        /// <summary>
+        /// Add a unique listener to the signal.
+        /// </summary>
+        public void AddListener(SignalDelegate listener) {
+            if(SendSignal == null) {
+                SendSignal += listener;
+            }
+            else if(!SendSignal.GetInvocationList().Contains(listener)) {
+                SendSignal += listener;
+            }
+        }
+
+        public void RemoveListener(SignalDelegate listener) {
+            SendSignal -= listener;
+        }
+
+        public void Dispatch(T item1, U item2, V item3) {
+            if(SendSignal != null) {
+                SendSignal(item1, item2, item3);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Signal with 4 parameters.
+    /// </summary>
+    public class Signal<T, U, V, W> {
+
+        public delegate void SignalDelegate(T item1, U item2, V item3, W item4);
+        private event SignalDelegate SendSignal;
+
+        /// <summary>
+        /// Add a unique listener to the signal.
+        /// </summary>
+        public void AddListener(SignalDelegate listener) {
+            if(SendSignal == null) {
+                SendSignal += listener;
+            }
+            else if(!SendSignal.GetInvocationList().Contains(listener)) {
+                SendSignal += listener;
+            }
+        }
+
+        public void RemoveListener(SignalDelegate listener) {
+            SendSignal -= listener;
+        }
+
+        public void Dispatch(T item1, U item2, V item3, W item4) {
+            if(SendSignal != null) {
+                SendSignal(item1, item2, item3, item4);
+            }
+        }
+
+    } 
+}
