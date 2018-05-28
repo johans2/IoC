d12f82f1122b88e88a7bb285b194c5d219060253
Johan Svensson <js@resolutiongames.com>
2018-05-28 10:10:41 +0200

    Remove the non monobehaviour implementation for IoC.

--- Assets/Cakewalk/IntegrationTests/ValidDependencies/TestBootStrapper.cs
+++ Assets/Cakewalk/IntegrationTests/ValidDependencies/TestBootStrapper.cs
@@ -5,42 +5,11 @@ using CakewalkIoC.Core;
 
 public class TestBootStrapper : BaseBootStrapper {
     
-    public TestBehaviour1 testprefab1;
-    public GameObject testprefab2;
-    public GameObject testprefab3;
-    public MonoBehaviour janne;
-
-
-
     public MonoBehaviour[] systems;
 
     public override void Configure(Container container) {
-        //container.RegisterPrefab<TestBehaviour1>(testprefab1);
-        /*
-        var prefabNameType = System.Reflection.Assembly.GetAssembly(typeof(TestBootStrapper)).GetType(janne.name);
-
-        var method = container.GetType().GetMethod("RegisterPrefab");
-        var generic = method.MakeGenericMethod(prefabNameType);
-        generic.Invoke(container, new object[] { janne.gameObject });
-
-        //container.RegisterPrefab <janne.GetType() > (janne);
-
-        container.RegisterPrefab<TestBehaviour2>(testprefab2);
-        container.RegisterPrefab<TestBehaviour3>(testprefab3);
-        */
-
         for(int i = 0; i < systems.Length; i++) {
             container.RegisterPrefab(systems[i]);
         }
     }
-    /*
-    private void OnValidate() {
-
-        Debug.Log("janne type=" + janne.GetType());
-        var prefabNameType = System.Reflection.Assembly.GetAssembly(typeof(TestBootStrapper)).GetType(janne.name);
-        Debug.Log("name=" + prefabNameType);
-        MonoBehaviour mb = (MonoBehaviour)(object)janne;
-        var c = mb.GetComponent(prefabNameType);
-        Debug.Log("Compontn=" + c);
-    }*/
 }
--- Assets/Cakewalk/IoC/Container.cs
+++ Assets/Cakewalk/IoC/Container.cs
@@ -10,71 +10,43 @@ namespace CakewalkIoC.Core {
 
     public class Container {
         
-        public Dictionary<Type, Type> Registrations { get; private set; }
         public Dictionary<Type, GameObject> PrefabRegistrations { get; private set; }
         public Dictionary<Type, object> Instances { get; private set; }
         
         private List<Type> dependencyChain;
 
         public Container() {
-            Registrations = new Dictionary<Type, Type>();
             Instances = new Dictionary<Type, object>();
             PrefabRegistrations = new Dictionary<Type, GameObject>();
             dependencyChain = new List<Type>();
         }
 
+        /// <summary>
+        /// Register a prefab available for injection. The prefab name must match the script you want to inject.
+        /// </summary>
+        /// <param name="mb">The prefab with attach Behaviour (cast to monobehaviour so we can extract the script from it).</param>
         public void RegisterPrefab(MonoBehaviour mb) {
-
             var prefabNameType = Assembly.GetAssembly(typeof(TestBootStrapper)).GetType(mb.name);
-            
-
             var registerMethod = GetType().GetMethod("RegisterPrefabWithBehaviour", BindingFlags.NonPublic | BindingFlags.Instance);
             var generic = registerMethod.MakeGenericMethod(prefabNameType);
             generic.Invoke(this, new object[] { mb.gameObject });
-
-
         }
 
         /// <summary>
-        /// Register a MonoBehaviour and its specified prefab.
+        /// Get the properties marked with [Dependency] attribute and inject them.
         /// </summary>
-        /// <typeparam name="TBehaviour">The specified MonoBehaviour available for injection.</typeparam>
-        /// <param name="prefab">The specified prefab the that will be instantiated with the MonoBehaviour attached to it.</param>
-        private void RegisterPrefabWithBehaviour<TBehaviour>(GameObject prefab) where TBehaviour : MonoBehaviour {
-            ValidatePrefabDependencies<TBehaviour>(prefab);
-            PrefabRegistrations.Add(typeof(TBehaviour), prefab);
-        }
+        /// <param name="obj">Object to inject into.</param>
+        public void InjectDependencies(object obj) {
+            Type type = obj.GetType();
 
-        /*
-        /// <summary>
-        /// Register a MonoBehaviour, its interface and its specified prefab.
-        /// </summary>
-        /// <typeparam name="TInterface">The specified Interface available for injection.</typeparam>
-        /// <typeparam name="TBehaviour">The MonoBehaviour implementation of the interface.</typeparam>
-        /// <param name="prefab">The specified prefab the that will be instantiated with the MonoBehaviour attached to it.</param>
-        public void RegisterPrefab<TInterface , TBehaviour>(GameObject prefab) where TBehaviour : MonoBehaviour, TInterface {
-            ValidatePrefabDependencies<TBehaviour>(prefab);
-            PrefabRegistrations.Add(typeof(TInterface), prefab);
-        }
-        */
-        private void ValidatePrefabDependencies<TBehaviour>(GameObject prefab) where TBehaviour : Component {
-            if(prefab == null) {
-                throw new GameObjectRegistrationException(string.Format("Registered dependency prefab {0} is null." , prefab));
-            }
+            PropertyInfo[] properties = GetDependencyProperties(type);
             
-            Component attachedBehaviour = prefab.GetComponent<TBehaviour>();
-            
-            if(attachedBehaviour == null) {
-                throw new GameObjectRegistrationException(string.Format("Prefab {0} has no behaviour of type {1} attached to it.", prefab.name, typeof(TBehaviour).ToString()));
-            }
-
-            CheckCircularDependencies(typeof(TBehaviour));
-
-            if(PrefabRegistrations.ContainsKey(typeof(TBehaviour))) {
-                throw new GameObjectRegistrationException(string.Format("Behaviour {0} (mapped to {1}) is already mapped to prefab {2}", typeof(TBehaviour).ToString(), prefab.name, PrefabRegistrations[typeof(TBehaviour)].name));
+            foreach(PropertyInfo property in properties) {
+                object instance = Resolve(property.PropertyType);
+                property.SetValue(obj, instance, null);
             }
         }
-
+        
         /// <summary>
         /// Check that the given Type has no circular dependencies. Throws CircularDependencyException() if it has.
         /// </summary>
@@ -98,44 +70,39 @@ namespace CakewalkIoC.Core {
 
             dependencyChain.Clear();
         }
-
+        
         /// <summary>
-        /// Register a type and its mapped interface for injection.
+        /// Register a MonoBehaviour and its specified prefab.
         /// </summary>
-        /// <typeparam name="TInterface">Mapped interface for registered type.</typeparam>
-        /// <typeparam name="TClass">Type to register.</typeparam>
-        public void Register<TInterface, TClass>() where TClass : class, TInterface {
-            ValidateRegistration<TInterface, TClass>();
-            Registrations.Add(typeof(TInterface), typeof(TClass));
+        /// <typeparam name="TBehaviour">The specified MonoBehaviour available for injection.</typeparam>
+        /// <param name="prefab">The specified prefab the that will be instantiated with the MonoBehaviour attached to it.</param>
+        private void RegisterPrefabWithBehaviour<TBehaviour>(GameObject prefab) where TBehaviour : MonoBehaviour {
+            ValidatePrefabDependencies<TBehaviour>(prefab);
+            PrefabRegistrations.Add(typeof(TBehaviour), prefab);
         }
-
+        
         /// <summary>
-        /// Register a Type for injection.
+        /// Validate that the registrated behaviour and prefab.
         /// </summary>
-        /// <typeparam name="TClass">Type to register.</typeparam>
-        public void Register<TClass>() where TClass : class {
-            if(Registrations.Values.Contains(typeof(TClass))) {
-                throw new RegistrationException(typeof(TClass).ToString() + "is already registered.");
+        /// <typeparam name="TBehaviour">Behaviour attached to the prefab.</typeparam>
+        /// <param name="prefab">Prefab.</param>
+        private void ValidatePrefabDependencies<TBehaviour>(GameObject prefab) where TBehaviour : Component {
+            if(prefab == null) {
+                throw new GameObjectRegistrationException(string.Format("Registered dependency prefab {0} is null." , prefab));
+            }
+            
+            Component attachedBehaviour = prefab.GetComponent<TBehaviour>();
+            
+            if(attachedBehaviour == null) {
+                throw new GameObjectRegistrationException(string.Format("Prefab {0} has no behaviour of type {1} attached to it.", prefab.name, typeof(TBehaviour).ToString()));
             }
 
-            Registrations.Add(typeof(TClass), typeof(TClass));
-        }
+            CheckCircularDependencies(typeof(TBehaviour));
 
-        /// <summary>
-        /// Get the properties marked with [Dependency] attribute and inject them.
-        /// </summary>
-        /// <param name="obj">Object to inject into.</param>
-        public void InjectDependencies(object obj) {
-            Type type = obj.GetType();
-
-            PropertyInfo[] properties = GetDependencyProperties(type);
-
-            foreach(PropertyInfo property in properties) {
-                object instance = Resolve(property.PropertyType);
-                property.SetValue(obj, instance, null);
+            if(PrefabRegistrations.ContainsKey(typeof(TBehaviour))) {
+                throw new GameObjectRegistrationException(string.Format("Behaviour {0} (mapped to {1}) is already mapped to prefab {2}", typeof(TBehaviour).ToString(), prefab.name, PrefabRegistrations[typeof(TBehaviour)].name));
             }
         }
-
         private PropertyInfo[] GetDependencyProperties(Type type) {
             PropertyInfo[] properties = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
                 .Where(prop => prop.IsDefined(typeof(Dependency), false)).ToArray();
@@ -154,16 +121,7 @@ namespace CakewalkIoC.Core {
                 throw new RegistrationException(typeof(TClass).ToString() + "is abstract and cannot be instantiated.");
             }
         }
-
-        /// <summary>
-        /// Returns an object of type T with all its constructor dependencies resolved. 
-        /// Mainly used for testing. Avoid using in game code. 
-        /// </summary>
-        /// <typeparam name="T">Type to resolve.</typeparam>
-        public object Resolve<T>() {
-            return Resolve(typeof(T));
-        }
-
+        
         /// <summary>
         /// Returns an object of type with all its constructor dependencies resolved. 
         /// </summary>
@@ -175,81 +133,25 @@ namespace CakewalkIoC.Core {
                 return instance;
             }
 
-            if(type.IsSubclassOf(typeof(MonoBehaviour))) {
-                // Instantiate the prefab
-                GameObject prefab;
-                if(!PrefabRegistrations.TryGetValue(type, out prefab)) {
-                    throw new NullBindingException("Attempt to instanciate a null-binding. " + type.ToString() + " is not registered to a prefab.");
-                }
-                
-                GameObject gameObject = GameObject.Instantiate(prefab);
-                GameObject.DontDestroyOnLoad(gameObject);
-                instance = gameObject.GetComponent(type);
-
-                if(GetDependencyProperties(type).Length == 0) {
-                    dependencyChain.Clear();
-                }
-                
+            // Instantiate the prefab
+            GameObject prefab;
+            if(!PrefabRegistrations.TryGetValue(type, out prefab)) {
+                throw new NullBindingException("Attempt to instanciate a null-binding. " + type.ToString() + " is not registered to a prefab.");
             }
-            else {
-                // Instantiate the object
-                Type implementation;
-                if(!Registrations.TryGetValue(type, out implementation)) {
-                    throw new NullBindingException("Attempt to instanciate a null-binding. " + type.ToString() + " is not registered.");
-                }
+                
+            GameObject gameObject = GameObject.Instantiate(prefab);
+            GameObject.DontDestroyOnLoad(gameObject);
+            instance = gameObject.GetComponent(type);
 
-                ConstructorInfo constructor = GetConstructor(implementation);
-                object[] dependencies = ResolvedDependencies(constructor);
-
-                if(dependencies.Length < 1) {
-                    dependencyChain.Clear();
-                }
-
-                instance = constructor.Invoke(dependencies);
+            if(GetDependencyProperties(type).Length == 0) {
+                dependencyChain.Clear();
             }
+                
             
             Instances.Add(type, instance);
             return instance;
         }
-
-        /// <summary>
-        /// Get the non-default constructor with the most parameters for an object.
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        private ConstructorInfo GetConstructor(Type type) {
-            ConstructorInfo[] constructors = type.GetConstructors();
-            if(constructors.Length < 1) {
-                throw new ResolveException(type.ToString() + " has no public constructors and cannot be instanciated.");
-            }
-            // Get constructor with the most parameters
-            ConstructorInfo correctConstructor = constructors.Aggregate((c1, c2) => c1.GetParameters().Count() > c2.GetParameters().Count() ? c1 : c2);
-            return correctConstructor;
-        }
-
-        /// <summary>
-        /// Get the resolved constructor-parameters for a given constructor.
-        /// </summary>
-        /// <param name="constructorInfo">The constructor to get and resolve parameters for.</param>
-        /// <returns></returns>
-        private object[] ResolvedDependencies(ConstructorInfo constructorInfo) {
-            List<object> parameters = new List<object>();
-
-            ParameterInfo[] parameterInfo = constructorInfo.GetParameters();
-
-            foreach(ParameterInfo parameter in parameterInfo) {
-                if(parameter.ParameterType.IsValueType) {
-                    throw new ResolveException("Parameter " + parameter.ToString() + " in " + constructorInfo.ReflectedType + " is a valuetype. This is not allowed in injected classes");
-                }
-
-                Type parameterType = parameter.ParameterType;
-                object instance = Resolve(parameterType);
-                parameters.Add(instance);
-            }
-
-            return parameters.ToArray();
-        }
-
+        
     }
 
 }
